be8fd5ad3c396e39829d2d287cf374c4eb7e0336
LiMing <1344030328@qq.com>
2023-01-04 10:50:57 +0800

    9th

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,270 +1,269 @@
-//package hw2;
-//
-//import edu.princeton.cs.algs4.StdOut;
-//import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class Percolation {
-//
-//    private WeightedQuickUnionUF uf;
-//    private boolean percolated;
-//    private final int N;
-//    private boolean[] isOpen;
-//    private boolean[] isFull;
-//    private List<Integer> openInTop;
-//    private List<Integer> openInBottom;
-//    private int numbersOfOpenSites;
-//
-//
-//    // create N-by-N grid, with all sites initially blocked
-//    public Percolation(int N) {
-//        if (N <= 0) {
-//            throw new IllegalArgumentException();
-//        }
-//
-//        uf = new WeightedQuickUnionUF(N * N);
-//        this.N = N;
-//        isOpen = new boolean[N * N];
-//        isFull = new boolean[N * N];
-//        openInTop = new ArrayList<>();
-//        openInBottom = new ArrayList<>();
-//        numbersOfOpenSites = 0;
-//        percolated = false;
-//    }
-//
-//    private int toIndex(int row, int col) {
-//        return row * N + col;
-//    }
-//
-//    /**
-//     * Return the indexes of the Adjacent to the given site.
-//     * @param row
-//     * @param col
-//     * @return
-//     */
-//    private int[] indexesOfAdjacent(int row, int col) {
-//        int[] ans;
-//        if (row > 0 && row < N - 1 && col > 0 && col < N - 1) {
-//            ans = new int[4];
-//            ans[0] = toIndex(row - 1, col);
-//            ans[1] = toIndex(row, col - 1);
-//            ans[2] = toIndex(row, col + 1);
-//            ans[3] = toIndex(row + 1, col);
-//        } else if (row == 0 && col != 0 && col != N - 1) {
-//            ans = new int[3];
-//            ans[0] = toIndex(row, col - 1);
-//            ans[1] = toIndex(row, col + 1);
-//            ans[2] = toIndex(row + 1, col);
-//        } else if (row == N - 1 && col != 0 && col != N - 1) {
-//            ans = new int[3];
-//            ans[0] = toIndex(row - 1, col);
-//            ans[1] = toIndex(row, col - 1);
-//            ans[2] = toIndex(row, col + 1);
-//        } else if (col == 0 && row != 0 && row != N - 1) {
-//            ans = new int[3];
-//            ans[0] = toIndex(row - 1, col);
-//            ans[1] = toIndex(row, col + 1);
-//            ans[2] = toIndex(row + 1, col);
-//        } else if (col == N - 1 && row != 0 && row != N - 1) {
-//            ans = new int[3];
-//            ans[0] = toIndex(row - 1, col);
-//            ans[1] = toIndex(row, col - 1);
-//            ans[2] = toIndex(row + 1, col);
-//        } else if (row == 0 && col == 0) {
-//            ans = new int[2];
-//            ans[0] = toIndex(row, col + 1);
-//            ans[1] = toIndex(row + 1, col);
-//        } else if (row == 0 && col == N - 1) {
-//            ans = new int[2];
-//            ans[0] = toIndex(row, col - 1);
-//            ans[1] = toIndex(row + 1, col);
-//        } else if (row == N - 1 && col == 0) {
-//            ans = new int[2];
-//            ans[0] = toIndex(row - 1, col);
-//            ans[1] = toIndex(row, col + 1);
-//        } else {
-//            ans = new int[2];
-//            ans[0] = toIndex(row - 1, col);
-//            ans[1] = toIndex(row, col - 1);
-//        }
-//        return ans;
-//    }
-//
-//    // open the site (row, col) if it is not open already
-//    public void open(int row, int col) {
-//        if (!(row >= 0 && row < N && col >= 0 && col < N)) {
-//            throw new IllegalArgumentException();
-//        }
-//
-//        if (isOpen(row, col)) {
-//            return;
-//        }
-//
-//        int index = toIndex(row, col);
-//        isOpen[index] = true;
-//        numbersOfOpenSites++;
-//
-//        if (row == 0) {
-//            openInTop.add(toIndex(row, col));
-//        //    isFull[index] = true;
-//        }
-//        if (row == N - 1) {
-//            openInBottom.add(toIndex(row, col));
-//        }
-//        //Connect the adjacent sites.
-//        int[] indexes = indexesOfAdjacent(row, col);
-//        for (int i = 0; i < indexes.length; i++) {
-//            if (isOpen[indexes[i]]) {
-//                uf.union(index, indexes[i]);
-//            }
-//        }
-//
-//    }
-//    // is the site (row, col) open?
-//    public boolean isOpen(int row, int col) {
-//        if (!(row >= 0 && row < N && col >= 0 && col < N)) {
-//            throw new IllegalArgumentException();
-//        }
-//        return isOpen[toIndex(row, col)];
-//    }
-//    // is the site (row, col) full?
-//    public boolean isFull(int row, int col) {
-//        if (!(row >= 0 && row < N && col >= 0 && col < N)) {
-//            throw new IllegalArgumentException();
-//        }
-//        int index = toIndex(row, col);
-//        if (isFull[index]) {
-//            return true;
-//        }
-//        for (int i = 0; i < openInTop.size(); i++) {
-//            if (uf.connected(openInTop.get(i), index)) {
-//                isFull[index] = true;
-//                break;
-//            }
-//        }
-//        return isFull[index];
-//    }
-//
-//    // number of open sites
-//    public int numberOfOpenSites() {
-//        return numbersOfOpenSites;
-//    }
-//    // does the system percolate?
-//    public boolean percolates() {
-//        if (percolated == true) {
-//            return true;
-//        }
-//
-//        for (int i = 0; i < openInBottom.size(); i++) {
-//            for (int j = 0; j < openInTop.size(); j++) {
-//                if (uf.connected(openInTop.get(j), openInBottom.get(i))) {
-//                    percolated = true;
-//                    return true;
-//                }
-//            }
-//        }
-//        return false;
-//
-//    }
-//    // use for unit testing (not required)
-//    public static void main(String[] args)  {
-//        Percolation p = new Percolation(5);
-//
-//        p.open(4, 1);
-//        p.open(3, 1);
-//        p.open(2, 1);
-//        p.open(1, 1);
-//        p.open(0, 2);
-//        StdOut.println(p.isFull(2, 1));
-//        StdOut.println(p.percolates());
-//        StdOut.println(p.numberOfOpenSites());
-//
-//    }
-//}
 package hw2;
 
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Percolation {
-    private int N;
-    private int[][] grid;
-    private int open_num = 0;
 
     private WeightedQuickUnionUF uf;
-    private int uf_bottom;
-    private WeightedQuickUnionUF ufWithoutBottom;  // To avoid backwash!
+    private boolean percolated;
+    private final int N;
+    private boolean[] isOpen;
+    private boolean[] isFull;
+    private List<Integer> openInTop;
+    private List<Integer> openInBottom;
+    private int numbersOfOpenSites;
 
-    /** Create N-by-N grid, with all sites initially blocked. */
+
+    // create N-by-N grid, with all sites initially blocked
     public Percolation(int N) {
         if (N <= 0) {
             throw new IllegalArgumentException();
         }
 
+        uf = new WeightedQuickUnionUF(N * N);
         this.N = N;
-        grid = new int[N][N];
-        uf = new WeightedQuickUnionUF(N * N + 2);
-        uf_bottom = N * N + 1;
-        ufWithoutBottom = new WeightedQuickUnionUF(N * N + 1);
+        isOpen = new boolean[N * N];
+        isFull = new boolean[N * N];
+        openInTop = new ArrayList<>();
+        openInBottom = new ArrayList<>();
+        numbersOfOpenSites = 0;
+        percolated = false;
     }
 
-    /** Open the site (row, col) if it is not open already. */
+    private int toIndex(int row, int col) {
+        return row * N + col;
+    }
+
+    /**
+     * Return the indexes of the Adjacent to the given site.
+     * @param row
+     * @param col
+     * @return
+     */
+    private int[] indexesOfAdjacent(int row, int col) {
+        int[] ans;
+        if (row > 0 && row < N - 1 && col > 0 && col < N - 1) {
+            ans = new int[4];
+            ans[0] = toIndex(row - 1, col);
+            ans[1] = toIndex(row, col - 1);
+            ans[2] = toIndex(row, col + 1);
+            ans[3] = toIndex(row + 1, col);
+        } else if (row == 0 && col != 0 && col != N - 1) {
+            ans = new int[3];
+            ans[0] = toIndex(row, col - 1);
+            ans[1] = toIndex(row, col + 1);
+            ans[2] = toIndex(row + 1, col);
+        } else if (row == N - 1 && col != 0 && col != N - 1) {
+            ans = new int[3];
+            ans[0] = toIndex(row - 1, col);
+            ans[1] = toIndex(row, col - 1);
+            ans[2] = toIndex(row, col + 1);
+        } else if (col == 0 && row != 0 && row != N - 1) {
+            ans = new int[3];
+            ans[0] = toIndex(row - 1, col);
+            ans[1] = toIndex(row, col + 1);
+            ans[2] = toIndex(row + 1, col);
+        } else if (col == N - 1 && row != 0 && row != N - 1) {
+            ans = new int[3];
+            ans[0] = toIndex(row - 1, col);
+            ans[1] = toIndex(row, col - 1);
+            ans[2] = toIndex(row + 1, col);
+        } else if (row == 0 && col == 0) {
+            ans = new int[2];
+            ans[0] = toIndex(row, col + 1);
+            ans[1] = toIndex(row + 1, col);
+        } else if (row == 0 && col == N - 1) {
+            ans = new int[2];
+            ans[0] = toIndex(row, col - 1);
+            ans[1] = toIndex(row + 1, col);
+        } else if (row == N - 1 && col == 0) {
+            ans = new int[2];
+            ans[0] = toIndex(row - 1, col);
+            ans[1] = toIndex(row, col + 1);
+        } else {
+            ans = new int[2];
+            ans[0] = toIndex(row - 1, col);
+            ans[1] = toIndex(row, col - 1);
+        }
+        return ans;
+    }
+
+    // open the site (row, col) if it is not open already
     public void open(int row, int col) {
-        if (!isOpen(row, col)) {
-            grid[row][col] = 1;
-            unionAround(row, col);
-            open_num += 1;
+        if (!(row >= 0 && row < N && col >= 0 && col < N)) {
+            throw new IllegalArgumentException();
         }
-    }
 
-    public boolean isOpen(int row, int col) {
-        validate(row, col);
-        return grid[row][col] == 1;
-    }
-
-    public boolean isFull(int row, int col) {
-        validate(row, col);
-        return ufWithoutBottom.connected(0, ufIndex(row, col));
-    }
-
-    public int numberOfOpenSites() {
-        return open_num;
-    }
-
-    public boolean percolates() {
-        return uf.connected(0, uf_bottom);
-    }
-
-    private void validate(int row, int col) {
-        if (row < 0 || row >= N || col < 0 || col >= N) {
-            throw new IndexOutOfBoundsException();
+        if (isOpen(row, col)) {
+            return;
         }
-    }
 
-    private int ufIndex(int row, int col) {
-        return row * N + col + 1;
-    }
+        int index = toIndex(row, col);
+        isOpen[index] = true;
+        numbersOfOpenSites++;
 
-    private int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
-    private void unionAround(int row, int col) {
-        for (int[] dir : directions) {
-            int x = row + dir[0];
-            int y = col + dir[1];
-            if (0 <= x && x < N && 0 <= y && y < N && isOpen(x, y)) {
-                uf.union(ufIndex(row, col), ufIndex(x, y));
-                ufWithoutBottom.union(ufIndex(row, col), ufIndex(x, y));
-            }
-        }
         if (row == 0) {
-            uf.union(0, ufIndex(row, col));
-            ufWithoutBottom.union(0, ufIndex(row, col));
+            openInTop.add(toIndex(row, col));
+        //    isFull[index] = true;
         }
         if (row == N - 1) {
-            uf.union(ufIndex(row, col), uf_bottom);
+            openInBottom.add(toIndex(row, col));
         }
+        //Connect the adjacent sites.
+        int[] indexes = indexesOfAdjacent(row, col);
+        for (int i = 0; i < indexes.length; i++) {
+            if (isOpen[indexes[i]]) {
+                uf.union(index, indexes[i]);
+            }
+        }
+
+    }
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        if (!(row >= 0 && row < N && col >= 0 && col < N)) {
+            throw new IllegalArgumentException();
+        }
+        return isOpen[toIndex(row, col)];
+    }
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        if (!(row >= 0 && row < N && col >= 0 && col < N)) {
+            throw new IllegalArgumentException();
+        }
+        int index = toIndex(row, col);
+        if (isFull[index]) {
+            return true;
+        }
+        for (int i = 0; i < openInTop.size(); i++) {
+            if (uf.connected(openInTop.get(i), index)) {
+                isFull[index] = true;
+                break;
+            }
+        }
+        return isFull[index];
     }
 
-    public static void main(String[] args) { }
+    // number of open sites
+    public int numberOfOpenSites() {
+        return numbersOfOpenSites;
+    }
+    // does the system percolate?
+    public boolean percolates() {
+        if (percolated == true) {
+            return true;
+        }
+
+        for (int i = 0; i < openInBottom.size(); i++) {
+            for (int j = 0; j < openInTop.size(); j++) {
+                if (uf.connected(openInTop.get(j), openInBottom.get(i))) {
+                    percolated = true;
+                    return true;
+                }
+            }
+        }
+        return false;
+
+    }
+    // use for unit testing (not required)
+    public static void main(String[] args)  {
+        Percolation p = new Percolation(5);
+
+        p.open(4, 1);
+        p.open(3, 1);
+        p.open(2, 1);
+        p.open(1, 1);
+        p.open(0, 2);
+        StdOut.println(p.isFull(2, 1));
+        StdOut.println(p.percolates());
+        StdOut.println(p.numberOfOpenSites());
+
+    }
 }
+//package hw2;
+//
+//import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+//
+//public class Percolation {
+//    private int N;
+//    private int[][] grid;
+//    private int open_num = 0;
+//
+//    private WeightedQuickUnionUF uf;
+//    private int uf_bottom;
+//    private WeightedQuickUnionUF ufWithoutBottom;  // To avoid backwash!
+//
+//    /** Create N-by-N grid, with all sites initially blocked. */
+//    public Percolation(int N) {
+//        if (N <= 0) {
+//            throw new IllegalArgumentException();
+//        }
+//
+//        this.N = N;
+//        grid = new int[N][N];
+//        uf = new WeightedQuickUnionUF(N * N + 2);
+//        uf_bottom = N * N + 1;
+//        ufWithoutBottom = new WeightedQuickUnionUF(N * N + 1);
+//    }
+//
+//    /** Open the site (row, col) if it is not open already. */
+//    public void open(int row, int col) {
+//        if (!isOpen(row, col)) {
+//            grid[row][col] = 1;
+//            unionAround(row, col);
+//            open_num += 1;
+//        }
+//    }
+//
+//    public boolean isOpen(int row, int col) {
+//        validate(row, col);
+//        return grid[row][col] == 1;
+//    }
+//
+//    public boolean isFull(int row, int col) {
+//        validate(row, col);
+//        return ufWithoutBottom.connected(0, ufIndex(row, col));
+//    }
+//
+//    public int numberOfOpenSites() {
+//        return open_num;
+//    }
+//
+//    public boolean percolates() {
+//        return uf.connected(0, uf_bottom);
+//    }
+//
+//    private void validate(int row, int col) {
+//        if (row < 0 || row >= N || col < 0 || col >= N) {
+//            throw new IndexOutOfBoundsException();
+//        }
+//    }
+//
+//    private int ufIndex(int row, int col) {
+//        return row * N + col + 1;
+//    }
+//
+//    private int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+//
+//    private void unionAround(int row, int col) {
+//        for (int[] dir : directions) {
+//            int x = row + dir[0];
+//            int y = col + dir[1];
+//            if (0 <= x && x < N && 0 <= y && y < N && isOpen(x, y)) {
+//                uf.union(ufIndex(row, col), ufIndex(x, y));
+//                ufWithoutBottom.union(ufIndex(row, col), ufIndex(x, y));
+//            }
+//        }
+//        if (row == 0) {
+//            uf.union(0, ufIndex(row, col));
+//            ufWithoutBottom.union(0, ufIndex(row, col));
+//        }
+//        if (row == N - 1) {
+//            uf.union(ufIndex(row, col), uf_bottom);
+//        }
+//    }
+//
+//    public static void main(String[] args) { }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -4,32 +4,27 @@ import edu.princeton.cs.introcs.StdStats;
 
 
 public class PercolationStats {
-    private int N;
-    private int T;
-    private double[] threshold;
+    // perform T independent experiments on an N-by-N grid
 
-    /**
-     * Perform T independent experiments on an N-by-N grid.
-     */
+    private double[] thresholds;
+    private double sqrtT;
+    private int N;
+
     public PercolationStats(int N, int T, PercolationFactory pf) {
-        if (T <= 0 || N <= 0) {
+        if (N <= 0 || T <= 0) {
             throw new IllegalArgumentException();
         }
-
-        this.T = T;
+        thresholds = new double[N];
         this.N = N;
-        threshold = new double[T];
+        sqrtT = Math.sqrt(T);
 
         /* Monte Carlo Simulation */
         for (int t = 0; t < T; t++) {
-            Percolation p = pf.make(N);
-            threshold[t] = simulate(p);
+            Percolation p = pf.make(T);
+            thresholds[t] = simulate(p);
         }
     }
 
-    /**
-     * Open randomly until the system percolates. Return the threshold.
-     */
     private double simulate(Percolation p) {
         int[] openOrder = StdRandom.permutation(N * N);
         for (int i = 0; !p.percolates(); i++) {
@@ -40,35 +35,81 @@ public class PercolationStats {
         return ((double) p.numberOfOpenSites()) / (N * N);
     }
 
-    private int[] coordinate(int index) {
-        return new int[]{index / N, index % N};
+    private int[] coordinate(int n) {
+        return new int[]{n / N, n % N};
     }
 
-    /**
-     * Sample mean of percolation threshold
-     */
+    // sample mean of percolation threshold
     public double mean() {
-        return StdStats.mean(threshold);
+        return StdStats.mean(thresholds);
     }
-
-    /**
-     * Sample standard deviation of percolation threshold
-     */
+    // sample standard deviation of percolation threshold
     public double stddev() {
-        return StdStats.stddev(threshold);
+        return StdStats.stddev(thresholds);
     }
-
-    /**
-     * Low endpoint of 95% confidence interval
-     */
+    // low endpoint of 95% confidence interval
     public double confidenceLow() {
-        return mean() - 1.96 * stddev() / Math.sqrt(T);
+        return mean() - 1.96 * stddev() / sqrtT;
     }
-
-    /**
-     * High endpoint of 95% confidence interval
-     */
+    // high endpoint of 95% confidence interval
     public double confidenceHigh() {
-        return mean() + 1.96 * stddev() / Math.sqrt(T);
+        return mean() + 1.96 * stddev() / sqrtT;
     }
 }
+
+//public class PercolationStats {
+//    private int N;
+//    private int T;
+//    private double[] threshold;
+//
+//    /** Perform T independent experiments on an N-by-N grid. */
+//    public PercolationStats(int N, int T, PercolationFactory pf) {
+//        if (T <= 0 || N <= 0) {
+//            throw new IllegalArgumentException();
+//        }
+//
+//        this.T = T;
+//        this.N = N;
+//        threshold = new double[T];
+//
+//        /* Monte Carlo Simulation */
+//        for (int t = 0; t < T; t++) {
+//            Percolation p = pf.make(N);
+//            threshold[t] = simulate(p);
+//        }
+//    }
+//
+//    /** Open randomly until the system percolates. Return the threshold. */
+//    private double simulate(Percolation p) {
+//        int[] openOrder = StdRandom.permutation(N * N);
+//        for (int i = 0; !p.percolates(); i++) {
+//            int x = coordinate(openOrder[i])[0];
+//            int y = coordinate(openOrder[i])[1];
+//            p.open(x, y);
+//        }
+//        return ((double) p.numberOfOpenSites()) / (N * N);
+//    }
+//
+//    private int[] coordinate(int index) {
+//        return new int[]{index / N, index % N};
+//    }
+//
+//    /** Sample mean of percolation threshold */
+//    public double mean() {
+//        return StdStats.mean(threshold);
+//    }
+//
+//    /** Sample standard deviation of percolation threshold */
+//    public double stddev() {
+//        return StdStats.stddev(threshold);
+//    }
+//
+//    /** Low endpoint of 95% confidence interval */
+//    public double confidenceLow() {
+//        return mean() - 1.96 * stddev() / Math.sqrt(T);
+//    }
+//
+//    /** High endpoint of 95% confidence interval */
+//    public double confidenceHigh() {
+//        return mean() + 1.96 * stddev() / Math.sqrt(T);
+//    }
